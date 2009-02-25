0681e161680503f43d23b947a89cf5e932bbb471
Arcadio Rubio García <arcadiorubiogarcia@gmail.com>
2009-02-25 13:58:07 +0100

    Property refactored for a more natural syntax in 1.8 & 1.9. Correct handling of properties with arity 0. Tests expanded accordingly

--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -4,29 +4,46 @@ require 'property'
 module PropertySpec
   describe 'property' do
     it 'should accept a simple property with correct arity' do
-      property :p1, [String, String] do |a, b|
+      p = property :p1 => [String, String] do |a, b|
         a + b == (b + a).reverse
       end
-    end
-
-    it 'should accept a simple property with arity = 0' do
-      property :p2 do
-        1 > 0
-      end
+      p.key.should == :p1
+      p.types.should == [String, String]
+      p.pred.call("a", "b").should be_true
     end
 
     it 'should accept a complex property with correct arity' do
-      property :p3, [String, String] do
-        predicate { |a, b| a + b == (b + a).reverse }
+      p = property :p2 => String do
+        predicate { |a| a.length == a.size }
       end
+      p.key.should == :p2
+      p.types.should == [String]
+      p.pred.call("aa").should be_true
     end
 
     it 'should not accept a simple property with incorrect arity' do
       lambda do
-        property :p4, [String] do |a, b|
+        property :p3 => [String] do |a, b|
           a + b == (b + a).reverse
         end
       end.should raise_error(ArgumentError)
+    end
+
+    it 'should not accept a complex property with incorrect arity' do
+      lambda do
+        property :p4 => [String] do |a, b|
+          predicate { |a, b| a + b == (b + a).reverse }
+        end
+      end.should raise_error(ArgumentError)
+    end
+
+    it 'should accept a simple property with arity = 0' do
+      p = property :p5 do
+        1 > 0
+      end
+      p.key.should == :p5
+      p.types.should == []
+      p.pred.call.should be_true
     end
   end
 end
--- src/property.rb
+++ src/property.rb
@@ -1,14 +1,16 @@
 def desc(doc); end
 
-def property(key, types = [], &block)
-  Property.new(key, types, &block)
+def property(sig, &block)
+  Property.new(sig, &block)
 end
 
 
 class Property
-  def initialize(key, types = [], &block)
-    @types = types
-    if block.arity > 0
+  attr_reader :key, :types, :pred
+
+  def initialize(sig, &block)
+    @key, @types = dump_s(sig)
+    if block.arity > 0 or @types.size == 0
       predicate(&block)
     else
       instance_eval(&block)
@@ -17,10 +19,32 @@ class Property
 
   def predicate(&expr)
     ts = @types.size
-    pa = expr.arity
+    pa = expr.arity != -1 ? expr.arity : 0
     if ts != pa
-      raise ArgumentError.new("The number of types (#{ts}) doesn't match the arity (#{pa}) of the predicate")
+      raise ArgumentError.new("The number of types (#{ts}) doesn't" +
+                              " match the arity (#{pa}) of the predicate")
     end
     @pred = expr
+  end
+
+  private
+
+  def dump_s(sig)
+    if sig.is_a?(Hash)
+      dump_h(sig)
+    elsif sig.is_a?(Symbol)
+      [sig, []]
+    else
+      raise ArgumentError.new("Incorrect property signature")
+    end
+  end
+
+  def dump_h(hash)
+    if hash.size != 1
+      raise ArgumentError.new("Incorrect property signature")
+    end
+    ary = hash.to_a
+    types = ary.first.last
+    [ary.first.first, types.is_a?(Array) ? types : [types]]
   end
 end
