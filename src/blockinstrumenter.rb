148af845524abe4bb89d998a6888da91df016fa2
Arcadio Rubio García <arcadiorubiogarcia@gmail.com>
2009-03-13 20:11:46 +0100

    New BlockInstrumenter skeleton

--- src/blockinstrumenter.rb
+++ src/blockinstrumenter.rb
@@ -1,47 +1,65 @@
 require 'property_core'
 require 'rubygems'
 require 'parse_tree'
+require 'parse_tree_extensions'
 require 'sexp_processor'
+require 'ruby2ruby'
 
 
 class BlockInstrumenter < SexpProcessor
   def initialize(property)
     super()
-    # self.strict = false
     self.auto_shift_type = true
-    process(ParseTree.new.parse_tree_for_proc(property.predicate))
+    self.strict = false
+    psexp = process(property.predicate.to_sexp)
+    pcode = Ruby2Ruby.new.process(psexp)
+    pblock = property.predicate.binding.eval(pcode)
+    property.predicate(&pblock)
+
+    # p b = .to_sexp
+    # p t = process(b)
+    # puts o = Ruby2Ruby.new.process(t)
   end
 
   def process_iter(exp)
-    exp.shift # call proc
-    exp.shift # masgn params
-    process exp.shift # body
-    s()
-  end
-
-  def process_block(exp)
-    while !exp.empty?
-      process exp.shift
-    end
-    s()
+    call = exp.shift
+    asign = exp.shift
+    body = process(exp.shift)
+    s(:iter, call, asign, body)
   end
 
   def process_call(exp)
-    process exp.shift
-    exp.shift # op
-    process exp.shift
-    s()
-  end
-
-  def process_dvar(exp)
-    # puts 'dvar'
-    exp.shift
-    s()
-  end
-
-  def process_array(exp) # call params
-    # puts 'array'
-    process exp.shift
-    s()
+    target = exp.shift
+    method = exp.shift
+    args = exp.shift
+    s(:call, target, method, args)
   end
 end
+
+  # def initialize(property)
+  #   super()
+  #   # self.strict = false
+  #   self.auto_shift_type = true
+  #   @tree = ParseTree.new.parse_tree_for_proc(property)
+  #   p @tree
+  #   @output = process(@tree)
+  # end
+
+  # def process_iter(exp)
+  #   call = exp.shift
+  #   asgn = exp.shift
+  #   body = process(exp.shift)
+  #   s(:iter, call, asgn, body)
+  # end
+
+  # def process_call(exp)
+  #   lvalue = process(exp.shift)
+  #   method = exp.shift
+  #   rvalue = exp.shift
+  #   rvalue = process(rvalue) if rvalue
+  #   unless rvalue
+  #     s(:call, lvalue, method)
+  #   else
+  #     s(:call, lvalue, method, rvalue) # problema con numeros y arrays
+  #   end
+  # end
