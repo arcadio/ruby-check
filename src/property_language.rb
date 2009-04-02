ca35796f4a27be8d2e1efa56692fabf02085c411
Arcadio Rubio García <arcadiorubiogarcia@gmail.com>
2009-04-02 18:29:24 +0200

    Signature rearranged

--- src/property_language.rb
+++ src/property_language.rb
@@ -13,6 +13,28 @@ module Kernel
 end
 
 
+module Signature
+  def dump_signature(signature)
+    if signature.is_a?(Hash)
+      dump_hash(signature)
+    else
+      [signature, []]
+    end
+  end
+
+  def dump_hash(hash)
+    if hash.size != 1
+      raise ArgumentError, 'incorrect property signature'
+    end
+    ary = hash.to_a
+    types = ary.first.last
+    [ary.first.first, types.is_a?(Array) ? types : [types]]
+  end
+
+  module_function :dump_signature, :dump_hash
+end
+
+
 class TrueClass
   def implies(conseq)
     !(conseq.nil? or conseq == false)
