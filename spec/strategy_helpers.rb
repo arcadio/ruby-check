902e14dc4b44e25c5bd17ac327de0eb27f4a7d14
Arcadio Rubio García <arcadiorubiogarcia@gmail.com>
2009-04-04 13:12:49 +0200

    Random strategy spec

--- spec/strategy_helpers.rb
+++ spec/strategy_helpers.rb
@@ -2,7 +2,7 @@ require 'strategy'
 
 
 module StrategyHelpers
-  shared_examples_for 'Strategy' do
+  shared_examples_for 'NewStrategy' do
     before(:each) do
       @strategy = strategy.new
     end
@@ -21,6 +21,46 @@ module StrategyHelpers
 
     it 'should refuse to report progress when just built' do
       lambda { @strategy.progress }.should raise_error
+    end
+  end
+
+
+  shared_examples_for 'Strategy' do
+    before(:each) do
+      Property.clear
+      @strategy = strategy.new
+      @strategy.set_property(define_prop)
+    end
+
+    after(:each) do
+      Property.clear
+      @strategy = nil
+    end
+  end
+
+
+  shared_examples_for 'PropertyWithoutCases' do
+    it 'should refuse to generate' do
+      lambda { @strategy.generate }.should raise_error
+    end
+
+    it 'should be exhausted' do
+      @strategy.exhausted?.should be_true
+    end
+
+    it 'should have 0 progress' do
+      @strategy.progress.should == 0
+    end
+  end
+
+
+  shared_examples_for 'PropertyWithCases' do
+    it 'should not be exhausted when just initialized' do
+      @strategy.exhausted?.should be_false
+    end
+
+    it 'should have not progressed when just initialized' do
+      @strategy.progress.should == 0
     end
   end
 end
