4d74d99dc730dadce74bf0825465a2b040245e91
Arcadio Rubio García <arcadiorubiogarcia@gmail.com>
2009-04-06 12:16:25 +0200

    Almost complete new call/cc implementation

--- src/exhaustive_strategy.rb
+++ src/exhaustive_strategy.rb
@@ -11,30 +11,47 @@ class ExhaustiveStrategy < Strategy
       @depth = 0
       @tuple = product(*@property.types)
       @resume = nil
+      @ret = nil
     end
   end
 
-  # tener en cuenta caso vacio tmb
   def gen
-    if @resume
-      resume, result = @resume.call
+    unless @resume
+      @resume, r = callcc do |c1|
+        @ret = c1
+        @tuple.exhaustive(@depth).each { |e| callcc { |c| @ret.call(c, e) } }
+      end
+      @depth += 1
     else
-      # utilizar callcc nuevo cada vez
-      resume, result = callcc do |c1|
-        @tuple.exhaustive(@depth).each do |e|
-          callcc { |c2| c1.call(c2, e) }
-        end
+      @resume, r = callcc do |c1|
+        @ret = c1
+        @resume.call
       end
     end
-    if !resume.is_a?(Continuation)
-      @depth += 1
-      @resume = nil
-      gen
-    else
-      @resume = resume
-      result
-    end
+    r
   end
+
+  # tener en cuenta caso vacio
+  # def gen
+  #   if @resume
+  #     resume, result = @resume.call
+  #   else
+  #     # utilizar callcc nuevo cada vez
+  #     resume, result = callcc do |c1|
+  #       @tuple.exhaustive(@depth).each do |e|
+  #         callcc { |c2| c1.call(c2, e) }
+  #       end
+  #     end
+  #   end
+  #   if !resume.is_a?(Continuation)
+  #     @depth += 1
+  #     @resume = nil
+  #     gen
+  #   else
+  #     @resume = resume
+  #     result
+  #   end
+  # end
 
   def can
     @property.types.all? { |t| t.respond_to?(:exhaustive) }
